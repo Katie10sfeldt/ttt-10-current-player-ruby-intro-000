b27e1d6a9518fb5dc9b30a8dce5b314993fe8c5f
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-16 18:33:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,15 @@
 def turn_count(board)
+  #must return the number of turns that have been taken
+  num_of_turns = 0
+
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      num_of_turns += 1
+    end
+  end
+  num_of_turns
 end
 
 def current_player(board)
+  #uses the '%' to determine whose turn it is. hint: turn count % 2 determines what is printed.
 end
